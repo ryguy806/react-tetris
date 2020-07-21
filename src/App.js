0af8e2f2734a3fc272bf57f10a7374ed12e204e8
ryguy806 <L1ghtSab3r11!!>
2020-07-21 15:38:15 -0700

    Replaced files with tutoral files.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import React from 'react';
 import Tetris from './components/Tetris';
 
 const App = () => (
-
   <div className="App">
     <Tetris />
   </div>
-)
+);
 
 export default App;
--- src/components/StartButton.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {StyledStartButton} from './styles/StyledStartButton';
-
-const StartButton = ({callBack}) => (
-    <StyledStartButton onClick={callBack}>Start Game</StyledStartButton>
-);
-
-export default StartButton;
