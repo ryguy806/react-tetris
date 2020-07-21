863f7fd45f3e37e10b8c60c2dba8dc0195085b33
ryguy806 <L1ghtSab3r11!!>
2020-07-21 11:47:29 -0700

    Added fuctions for peices and stage

---
+++ src/gameHelpers.js
@@ -0,0 +1,12 @@
+export const STAGE_WIDTH = 12;
+export const STAGE_HEIGHT = 20;
+
+export const createStage = () => 
+    Array.from(Array(STAGE_HEIGHT), () => 
+        new Array(STAGE_WIDTH).fill([0, 'clear'])
+    )
+
+/* 
+    0 = nothing, each tetromino will have a letter in that particular cell
+    clear = merge when a tetromino colides on the stage.
+*/
